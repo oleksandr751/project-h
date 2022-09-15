7d91d1fb248e0c2ec227bdef174bd9c9c05b941f
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-09-15 18:00:35 +0200

    add: content list page stylying

--- src/pages/countries_list/index.tsx
+++ src/pages/countries_list/index.tsx
@@ -3,32 +3,51 @@ import { countries } from "../../data/countries";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { MainContext } from "../../context/index";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { countriesListStyle } from "./styles";
+import ContentCard from "./components/contentCard";
+import { IContentData } from "../../interfaces/index";
 
 const CountriesListPage = () => {
   const { setSelectedItem } = useContext(MainContext);
+  const handleClick = (country: IContentData) => {
+    setSelectedItem(country);
+    localStorage.setItem("id", country.id);
+    console.log(
+      countries.find((obj) => {
+        return obj["id"] === localStorage.getItem("id");
+      })
+    );
+  };
   return (
-    <Box style={{ display: "flex", flexDirection: "column" }}>
-      {countries.map((country) => (
-        <Link
-          key={country.id}
-          to={"/timeline"}
-          style={{
-            textDecoration: "none",
-          }}
-          onClick={() => {
-            setSelectedItem(country);
-            localStorage.setItem("id", country.id);
-            console.log(
-              countries.find((obj) => {
-                return obj["id"] === localStorage.getItem("id");
-              })
-            );
-          }}
-        >
-          {country.name}
-        </Link>
-      ))}
+    <Box style={countriesListStyle.mainDiv as React.CSSProperties}>
+      <Box style={countriesListStyle.mainChildDiv as React.CSSProperties}>
+        <Box style={countriesListStyle.leftChildDiv as React.CSSProperties}>
+          <Typography variant="h1" sx={countriesListStyle.typography}>
+            Welcome!
+          </Typography>
+          <Typography variant="h5" sx={countriesListStyle.typography}>
+            Please select a topic to start with
+          </Typography>
+        </Box>
+        <Box sx={countriesListStyle.rightChildDiv as React.CSSProperties}>
+          {countries.map((country) => (
+            <Link
+              key={country.id}
+              to={"/timeline"}
+              style={{
+                textDecoration: "none",
+                marginTop: "20px",
+              }}
+              onClick={() => {
+                handleClick(country);
+              }}
+            >
+              <ContentCard data={country}></ContentCard>
+            </Link>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
---
+++ src/pages/countries_list/styles/index.ts
@@ -0,0 +1,40 @@
+import background1 from "../../../images/background1.jpg";
+export const countriesListStyle = {
+  mainDiv: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundImage: `url(${background1})`,
+    backgroundSize: "100%",
+    backgroundColor: "black",
+  },
+  mainChildDiv: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "70%",
+    width: "70%",
+  },
+  leftChildDiv: {
+    display: "flex",
+    padding: "40px",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "50%",
+  },
+  rightChildDiv: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "50%",
+    width: "50%",
+    borderLeft: 5,
+    borderColor: "#e0fbfc ",
+  },
+  typography: { color: "#e0fbfc", marginTop: "20px", userSelect: "none" },
+};
