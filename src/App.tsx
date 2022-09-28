92c79d2b5f7b9eef29896d1caa0ab4fa86ffe413
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-09-29 00:44:55 +0200

    edit: app clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 import { MainContext } from "./context";
 import MainRoutes from "./routes";
 import { ThemeProvider } from "@mui/material/styles";
-import { countries } from "./data/countries";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { globalTheme } from "./styles/theme";
 import { PASS, SESSION_KEY } from "./config";
 
 function App() {
-  const sortedCountries = countries.sort((a, b) => (a.name < b.name ? -1 : 1));
-  const [countriesData, setCountriesData] = useState(sortedCountries);
-  const [selectedItem, setSelectedItem] = useState(countries[0]);
+  const [countriesData, setCountriesData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState({});
   const [isAuthenticated, setIsAuthenticated] = useState(
     window.sessionStorage.getItem(SESSION_KEY) === PASS
   );
-  useEffect(() => {
-    const initialCountry = countries.find(
-      (obj) => obj["id"] === localStorage.getItem("id")
-    );
-    initialCountry ? setSelectedItem(initialCountry) : console.log("error");
-  }, []);
+  const [greatPeople, setGreatPeople] = useState([]);
   return (
     <>
       <MainContext.Provider
         value={{
+          greatPeople,
+          setGreatPeople,
           countriesData,
           setCountriesData,
           isAuthenticated,
