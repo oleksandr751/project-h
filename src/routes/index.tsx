e113d2427ffb2613de480f04c8b566e219aa9ad1
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-09-29 00:51:33 +0200

    edit: content page props removed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CountriesListPage from "../pages/countries_list";
 import ContentPage from "../pages/content";
-import { MainContext } from "../context/index";
-import { useContext } from "react";
 
 const MainRoutes = () => {
-  const { selectedItem } = useContext(MainContext);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CountriesListPage />}></Route>
-        <Route path="/timeline" element={<ContentPage data={selectedItem} />} />
+        <Route path="/timeline" element={<ContentPage />} />
       </Routes>
     </BrowserRouter>
   );
