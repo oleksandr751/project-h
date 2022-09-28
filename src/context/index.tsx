5d08bc1c39000caaddf69edf9bc85e8dd3837d64
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-09-29 00:47:17 +0200

    edit: MainContext types

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,20 +1,24 @@
 import { createContext } from "react";
 import { countries } from "../data/countries";
-import { IContentData } from "../interfaces";
+import { IContentData, IGreatPeople } from "../interfaces";
 
 interface IMainContext {
   countriesData: IContentData[];
-  selectedItem: IContentData;
+  selectedItem: IContentData | any;
   isAuthenticated: boolean;
+  greatPeople: IGreatPeople[];
+  setGreatPeople: (par: IGreatPeople[] | any) => void;
   setIsAuthenticated: (par: boolean) => void;
   setSelectedItem: (par: IContentData) => void;
-  setCountriesData: (par: IContentData[]) => void;
+  setCountriesData: (par: IContentData[] | any) => void;
 }
 
 export const MainContext = createContext<IMainContext>({
-  countriesData: countries,
+  countriesData: [],
   selectedItem: countries[0],
   isAuthenticated: false,
+  greatPeople: [],
+  setGreatPeople: () => {},
   setIsAuthenticated: () => {},
   setSelectedItem: () => {},
   setCountriesData: () => {},
