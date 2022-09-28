3b1008cfea61f24b80e02e28b99121f3c2b1f7eb
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-09-29 00:46:08 +0200

    add: secondary loading image

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import loadImage from "../../images/loading.gif";
+import loadImage2 from "../../images/loading2.gif";
 import { Box } from "@mui/material";
-const Loading = () => {
+interface ILoading {
+  type: string;
+}
+const Loading: React.FC<ILoading> = ({ type }) => {
   return (
     <Box>
       <Box>
-        <img alt="Loading...." src={loadImage}></img>
+        <img
+          alt="Loading...."
+          src={type === "primary" ? loadImage : loadImage2}
+        ></img>
       </Box>
     </Box>
   );
