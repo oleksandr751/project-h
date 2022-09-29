dd38b6854c7ecba48b80d8b0096d22de6daecccd
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-09-29 17:21:27 +0200

    add: code to greatpeople root file

--- src/pages/content/components/great_people/index.tsx
+++ src/pages/content/components/great_people/index.tsx
@@ -1,8 +1,13 @@
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import React, { useState } from "react";
 import ContentCard from "../../../../components/contentCard/contentCard";
 import { IGreatPeople } from "../../../../interfaces";
 import SearchBar from "../../../../components/searchBar/index";
+import AddIcon from "@mui/icons-material/Add";
+import { MainContext } from "../../../../context/index";
+import { useContext } from "react";
+import GreatPersonForm from "./greatPersonForm";
+import AlertComponent from "../../../../components/alert/index";
 
 interface IGreatPeopleComponent {
   data: IGreatPeople[];
@@ -10,6 +15,12 @@ interface IGreatPeopleComponent {
 
 const GreatPeople: React.FC<IGreatPeopleComponent> = ({ data }) => {
   const [searchBarText, setSearchBarText] = useState("");
+  const [openAlert, setOpenAlert] = useState(false);
+  const [addAttempt, setAddAttempt] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("alerMessage");
+  const { isAuthenticated, selectedItem, editAttempt } =
+    useContext(MainContext);
   return (
     <Box
       sx={{
@@ -19,17 +30,64 @@ const GreatPeople: React.FC<IGreatPeopleComponent> = ({ data }) => {
         alignItems: "center",
       }}
     >
-      <SearchBar
-        searchBarText={searchBarText}
-        setSearchBarText={setSearchBarText}
+      <AlertComponent
+        text={alertMessage}
+        type={alertType}
+        open={openAlert}
+        setOpen={setOpenAlert}
       />
-      {data
-        .filter((country) =>
-          country.name.toLowerCase().includes(searchBarText.toLowerCase())
-        )
-        .map((person, idx) => (
-          <ContentCard personData={person} type={"person"} key={idx} />
-        ))}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <SearchBar
+          searchBarText={searchBarText}
+          setSearchBarText={setSearchBarText}
+        />
+        {isAuthenticated && !editAttempt && (
+          <IconButton
+            sx={{ marginLeft: "15px" }}
+            onClick={() => {
+              setAddAttempt(true);
+            }}
+          >
+            <AddIcon />
+          </IconButton>
+        )}
+      </Box>
+      {addAttempt ? (
+        <GreatPersonForm
+          data={selectedItem}
+          setAddAttempt={setAddAttempt}
+          setOpenAlert={setOpenAlert}
+          setAlertMessage={setAlertMessage}
+          setAlertType={setAlertType}
+          action="add"
+        />
+      ) : editAttempt ? (
+        <GreatPersonForm
+          data={selectedItem}
+          setAddAttempt={setAddAttempt}
+          setOpenAlert={setOpenAlert}
+          setAlertMessage={setAlertMessage}
+          setAlertType={setAlertType}
+          action="edit"
+        />
+      ) : (
+        <>
+          {data
+            .filter((country) =>
+              country.name.toLowerCase().includes(searchBarText.toLowerCase())
+            )
+            .map((person, idx) => (
+              <ContentCard personData={person} type={"person"} key={idx} />
+            ))}
+        </>
+      )}
     </Box>
   );
 };
