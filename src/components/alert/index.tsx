2f8d0b87b88e83d1aa043281dfaab5db1636e1c3
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-09-29 17:17:52 +0200

    add: alert component

---
+++ src/components/alert/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Box, Collapse, Alert, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+interface IAlertComponent {
+  text: string;
+  type: any;
+  open: boolean;
+  setOpen: (par: boolean) => void;
+}
+const AlertComponent: React.FC<IAlertComponent> = ({
+  text,
+  type,
+  open,
+  setOpen,
+}) => {
+  return (
+    <Box>
+      <Collapse in={open} orientation="vertical" collapsedSize={"0px"}>
+        <Alert
+          severity={type}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2, width: "200px" }}
+        >
+          {text}
+        </Alert>
+      </Collapse>
+    </Box>
+  );
+};
+
+export default AlertComponent;
