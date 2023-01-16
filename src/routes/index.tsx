bacf89905882f167f4f823f8f442e96e50a7ef01
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2023-01-16 21:19:39 +0100

    edit: fetching moved to maincontext level

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CountriesListPage from "../pages/countries_list";
 import ContentPage from "../pages/content";
+import ArticleCreation from "../pages/article/create";
+import { useCountriesData } from "../hooks/countries.hook";
+import { useEffect } from "react";
 
 const MainRoutes = () => {
+  const { getCountries, getCountry } = useCountriesData();
+
+  useEffect(() => {
+    getCountries();
+    getCountry(localStorage.getItem("id"));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CountriesListPage />}></Route>
         <Route path="/country" element={<ContentPage />} />
+        <Route path="/article/create" element={<ArticleCreation />} />
       </Routes>
     </BrowserRouter>
   );
