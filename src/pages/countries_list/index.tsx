e6245b5a6d04ac7437e69a4ad0f6c4440f673974
denisN2102 <99597626+denisN2102@users.noreply.github.com>
2023-01-30 18:12:56 +0200

    update: component footer to CountriesListPage

--- src/pages/countries_list/index.tsx
+++ src/pages/countries_list/index.tsx
@@ -11,6 +11,7 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import SearchBar from "../../components/searchBar";
 import Loading from "../../components/loading";
 import AuthComponent from "../../components/auth";
+import Footer from "../../components/footer";
 
 const CountriesListPage = () => {
   const { setSelectedItem, countriesData } = useContext(MainContext);
@@ -23,83 +24,90 @@ const CountriesListPage = () => {
   };
 
   return (
-    <Box sx={countriesListStyle.mainDiv as React.CSSProperties}>
-      <Box sx={countriesListStyle.iconList}>
-        <IconList
-          loginAttempt={loginAttempt}
-          setLoginAttempt={setLoginAttempt}
-        />
-      </Box>
-      <Box sx={countriesListStyle.mainChildDiv as React.CSSProperties}>
-        <Box sx={countriesListStyle.leftChildDiv as React.CSSProperties}>
-          {loginAttempt ? (
-            <>
-              <Typography variant="h3" sx={countriesListStyle.typography}>
-                Sign in to receive admin privileges
-              </Typography>
-              <Typography variant="h5" sx={countriesListStyle.typography}>
-                Press arrow to go back
-              </Typography>
-              <IconButton
-                onClick={() => {
-                  setLoginAttempt(false);
-                }}
-              >
-                <ArrowBackIcon
-                  sx={{ fontSize: "50px", color: "white" }}
-                ></ArrowBackIcon>
-              </IconButton>
-            </>
-          ) : (
-            <>
-              <Typography variant="h1" sx={countriesListStyle.typography}>
-                Welcome!
-              </Typography>
-              <Typography variant="h5" sx={countriesListStyle.typography}>
-                Please select a topic to start with
-              </Typography>
-            </>
-          )}
+    <Box>
+      <Box sx={countriesListStyle.mainDiv as React.CSSProperties}>
+        <Box sx={countriesListStyle.iconList}>
+          <IconList
+            loginAttempt={loginAttempt}
+            setLoginAttempt={setLoginAttempt}
+          />
         </Box>
-        <Box sx={countriesListStyle.rightChildDiv as React.CSSProperties}>
-          {loginAttempt ? (
-            <AuthComponent setLoginAttempt={setLoginAttempt}></AuthComponent>
-          ) : (
-            <>
-              <SearchBar
-                searchBarText={searchBarText}
-                setSearchBarText={setSearchBarText}
-              />
-              <Box sx={countriesListStyle.countriesList as React.CSSProperties}>
-                {loading ? (
-                  <Loading type="secondary" />
-                ) : (
-                  <>
-                    {countriesData
-                      .filter((country) =>
-                        country.name
-                          .toLowerCase()
-                          .includes(searchBarText.toLowerCase())
-                      )
-                      .map((country) => (
-                        <Link
-                          key={country.id}
-                          to={"/country"}
-                          style={countriesListStyle.link as React.CSSProperties}
-                          onClick={() => {
-                            handleClick(country);
-                          }}
-                        >
-                          <ContentCard countryData={country} type="country" />
-                        </Link>
-                      ))}
-                  </>
-                )}
-              </Box>
-            </>
-          )}
+        <Box sx={countriesListStyle.mainChildDiv as React.CSSProperties}>
+          <Box sx={countriesListStyle.leftChildDiv as React.CSSProperties}>
+            {loginAttempt ? (
+              <>
+                <Typography variant="h3" sx={countriesListStyle.typography}>
+                  Sign in to receive admin privileges
+                </Typography>
+                <Typography variant="h5" sx={countriesListStyle.typography}>
+                  Press arrow to go back
+                </Typography>
+                <IconButton
+                  onClick={() => {
+                    setLoginAttempt(false);
+                  }}
+                >
+                  <ArrowBackIcon
+                    sx={{ fontSize: "50px", color: "white" }}
+                  ></ArrowBackIcon>
+                </IconButton>
+              </>
+            ) : (
+              <>
+                <Typography variant="h1" sx={countriesListStyle.typography}>
+                  Welcome!
+                </Typography>
+                <Typography variant="h5" sx={countriesListStyle.typography}>
+                  Please select a topic to start with
+                </Typography>
+              </>
+            )}
+          </Box>
+          <Box sx={countriesListStyle.rightChildDiv as React.CSSProperties}>
+            {loginAttempt ? (
+              <AuthComponent setLoginAttempt={setLoginAttempt}></AuthComponent>
+            ) : (
+              <>
+                <SearchBar
+                  searchBarText={searchBarText}
+                  setSearchBarText={setSearchBarText}
+                />
+                <Box
+                  sx={countriesListStyle.countriesList as React.CSSProperties}
+                >
+                  {loading ? (
+                    <Loading type="secondary" />
+                  ) : (
+                    <>
+                      {countriesData
+                        .filter((country) =>
+                          country.name
+                            .toLowerCase()
+                            .includes(searchBarText.toLowerCase())
+                        )
+                        .map((country) => (
+                          <Link
+                            key={country.id}
+                            to={"/country"}
+                            style={
+                              countriesListStyle.link as React.CSSProperties
+                            }
+                            onClick={() => {
+                              handleClick(country);
+                            }}
+                          >
+                            <ContentCard countryData={country} type="country" />
+                          </Link>
+                        ))}
+                    </>
+                  )}
+                </Box>
+              </>
+            )}
+          </Box>
         </Box>
       </Box>
+      <Footer />
     </Box>
   );
 };
