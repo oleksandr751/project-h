1383a9e7a942f5cd45f0953aa0f82ca8d7043fc4
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2023-02-01 18:57:39 +0100

    add: article mock up data

---
+++ src/data/articleCardsData.ts
@@ -0,0 +1,46 @@
+export const articleData = [
+  {
+    name: "Battle of Britain",
+    description:
+      "The primary objective of the German forces was to compel Britain to agree to a negotiated peace settlement. In July 1940...",
+    articleId: 1,
+    imageSrc: "https://www.historyhit.com/app/uploads/2020/07/SpitIX611b-1.jpg",
+    tags: ["Military", "Science"],
+    upvotes: 118,
+    downvotes: 11,
+    author: "Martin Septim",
+  },
+  {
+    name: "Battle of Britain",
+    description:
+      "The primary objective of the German forces was to compel Britain to agree to a negotiated peace settlement. In July 1940...",
+    articleId: 1,
+    imageSrc: "https://www.historyhit.com/app/uploads/2020/07/SpitIX611b-1.jpg",
+    tags: ["Military", "Science"],
+    upvotes: 118,
+    downvotes: 11,
+    author: "Martin Septim",
+  },
+  {
+    name: "Battle of Britain",
+    description:
+      "The primary objective of the German forces was to compel Britain to agree to a negotiated peace settlement. In July 1940...",
+    articleId: 1,
+    imageSrc: "https://www.historyhit.com/app/uploads/2020/07/SpitIX611b-1.jpg",
+    tags: ["Military", "Science"],
+    upvotes: 118,
+    downvotes: 11,
+    author: "Martin Septim",
+  },
+  {
+    name: "Battle of Britain",
+    description:
+      "The primary objective of the German forces was to compel Britain to agree to a negotiated peace settlement. In July 1940...",
+    articleId: 1,
+    imageSrc: "https://www.historyhit.com/app/uploads/2020/07/SpitIX611b-1.jpg",
+    tags: ["Military", "Science"],
+    upvotes: 118,
+    downvotes: 11,
+    author: "Martin Septim",
+  },
+];
