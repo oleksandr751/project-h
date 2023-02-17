5bc75e6b4ad6ece0ef16126e1578c63d3e971cd9
denisN2102 <99597626+denisN2102@users.noreply.github.com>
2023-02-17 19:20:01 +0200

    PH-14: Research and implement formattable text in article edit component (#9)
    * add: formattable text component
    
    * edit: styles

---
+++ src/pages/content/textEditor/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useQuill } from "react-quilljs";
+import "react-quill/dist/quill.snow.css";
+import { Box } from "@mui/material";
+import ReactQuill from "react-quill";
+
+const TextEditor = () => {
+  const { quill, quillRef } = useQuill();
+  const [value, setValue] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+    ],
+  };
+
+  React.useEffect(() => {
+    if (quill) {
+      quill.on("text-change", () => {
+        console.log(quillRef.current.firstChild.innerHTML);
+        setValue(quillRef.current.firstChild.innerHTML);
+      });
+    }
+  }, [quill]);
+  console.log(value, "value");
+
+  return (
+    <Box>
+      <Box>
+        {/* <Box ref={quillRef} /> */}
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          modules={modules}
+          style={{
+            width: 600,
+            height: 400,
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default TextEditor;
