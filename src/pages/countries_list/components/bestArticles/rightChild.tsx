be79348f5792bee58699b0b12317d917fedfc21e
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2023-02-27 21:03:00 +0100

    delete: removed unnecessary header

--- src/pages/countries_list/components/bestArticles/rightChild.tsx
+++ src/pages/countries_list/components/bestArticles/rightChild.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "../../../../components/slider";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { cardStyles } from "../../../../components/articleCard/styles";
 import { articleData } from "../../../../data/articleCardsData";
 const RightChild = () => {
@@ -13,9 +13,6 @@ const RightChild = () => {
         alignItems: "center",
       }}
     >
-      <Box>
-        <Typography variant="h4">Best rated articles</Typography>
-      </Box>
       <Box sx={cardStyles.contentDiv}>
         <Slider slidesData={articleData} />
       </Box>
